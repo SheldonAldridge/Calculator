a847240b19b01fda39c033d9b2ab889e890154f8
Sheldon <sheldonaldridge@yahoo.com>
2022-04-29 01:19:19 +0200

    Update main.js
    setting up for calculation for buttons

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*********************
+Setting up varlaibles*
+**********************/            
+
+let numbers = document.querySelectorAll('.col-num');
+let operators = document.querySelectorAll('.col-op');
+let endOperators = document.querySelectorAll('.col-op-end');
+let calculation = document.querySelector('.calc');
+let result = document.querySelector('.answer');
+let num1
+let num2
+
+/*****************************************
+Function for numbers to display on screen*
+******************************************/  
+
+function buttonPress(){
+
+}
