d7d354b150cedb982bb4037aeb51f63551dfd7ba
Sheldon <sheldonaldridge@yahoo.com>
2022-04-29 01:19:02 +0200

    Update time.js
    updated the js so that the time tracks in realtime

--- js/time.js
+++ js/time.js
@@ -1,3 +1,11 @@
+
+
+function setRealTime(){
+    let refresh = 1000;
+    let refreshTime = setTimeout('showTime()',refresh);
+}
+
+function showTime(){
 let times = document.querySelector(".time")
 
 const currentTime = new Date()
@@ -5,10 +13,15 @@ const currentTime = new Date()
 let hour = currentTime.getHours()
 let minute = currentTime.getMinutes()
 
+
 hour = checkTime(hour)
 minute = checkTime(minute)
 
-times.innerHTML += `${hour}:${minute}`
+times.innerHTML = `${hour}:${minute}`
+setRealTime()
+}
+
+showTime()
 
 function checkTime(i)
 {
