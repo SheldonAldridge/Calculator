daee723598b387a9b283cc5b43e53ffe0b3dd69f
Sheldon <sheldonaldridge@yahoo.com>
2022-04-29 23:38:25 +0200

    Update main.js
    numbers reflecting on screen with button press

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 /*********************
 Setting up varlaibles*
-**********************/            
+**********************/
 
-let numbers = document.querySelectorAll('.col-num');
-let operators = document.querySelectorAll('.col-op');
-let endOperators = document.querySelectorAll('.col-op-end');
-let calculation = document.querySelector('.calc');
-let result = document.querySelector('.answer');
-let num1
-let num2
+let result;
+let calculation;
+let num1;
+let num2;
+
 
 /*****************************************
 Function for numbers to display on screen*
-******************************************/  
+******************************************/
 
-function buttonPress(){
+function buttonPress(numbers) {
 
+    result = document.querySelector('.answer');
+    let input=result.innerHTML += numbers
+    console.log(input)
+
+    if(input.length >= 21)
+    {
+        
+        result.innerHTML += "\n";
+        document.querySelector('.answer').style.marginTop = "-1rem";
+    }
 }
